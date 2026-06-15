a9dc02379e1b3a38c4efb78aaca9f28c8174a957
Amey <ameymvaidya@gmail.com>
2026-06-15 22:50:21 +0530

    Fix Issue #70: Clear voice status message on stop listening

--- lib/voice/voice_commands.dart
+++ lib/voice/voice_commands.dart
@@ -138,6 +138,7 @@ class VoiceCommandService extends ChangeNotifier {
     _actionTimer?.cancel();
     await _speech.stop();
     _isListening = false;
+    _statusMessage = '';
     debugPrint('VoiceCommandService.stopListening: stopped');
     notifyListeners();
   }
